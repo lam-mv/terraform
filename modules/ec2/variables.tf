b3371ec68490e6f05cd5ceffb975f93326549e3f
lam-mv <lam.mv@terra-drone.co.jp>
2022-09-09 00:25:50 +0700

    [Edit] refactor folder

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,14 @@
+variable "ec2_name" {
+  type    = string
+  description = "Name of EC2"
+}
+
+variable "instance_type" {
+  type = string
+  description = "Instance type of the EC2"
+  
+  validation {
+    condition = contains(["t2.micro", "t3.small"], var.instance_type)
+    error_message = "Value not allow."
+  }
+}
