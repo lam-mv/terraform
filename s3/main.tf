6a01b25ea2d413efe71e8dae870118cf52efd77e
lam-mv <lam.mv@terra-drone.co.jp>
2022-09-08 23:59:11 +0700

    [Add] Add policy for s3

--- s3/main.tf
+++ s3/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_s3_bucket" "terraform-bucket" {
   bucket = "terraform-series-bucket"
-
+  policy = file("s3_static_policy.json")
   tags = {
     Name        = "Terraform s3"
   }
