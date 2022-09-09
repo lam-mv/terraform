23678c1acba6a77e3d83d80acd247eecd838ca18
lam-mv <lam.mv@terra-drone.co.jp>
2022-09-09 09:42:52 +0700

    [Fix] Fix error when validate

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -3,16 +3,15 @@
 resource "aws_vpc" "vpc" {
   cidr_block = var.vpc_cidr_block
   enable_dns_hostnames = true
-  
+
     tags = {
       "Name" = "mvl_vpc"
   }
 }
 
-
 resource "aws_subnet" "private_subnet" {
   count = length(var.private_subnet)
-  
+
   vpc_id     = aws_vpc.vpc.id
   cidr_block = var.private_subnet[count.index]
   availability_zone = var.availability_zone[count.index % length(var.availability_zone)]
@@ -24,11 +23,11 @@ resource "aws_subnet" "private_subnet" {
 
 resource "aws_subnet" "public_subnet" {
   count = length(var.public_subnet)
-  
+
   vpc_id     = aws_vpc.vpc.id
   cidr_block = var.public_subnet[count.index]
   availability_zone = var.availability_zone[count.index % length(var.availability_zone)]
-  
+
   tags = {
     "Name" = "public-subnet"
   }
@@ -36,7 +35,7 @@ resource "aws_subnet" "public_subnet" {
 
 resource "aws_internet_gateway" "ig" {
   vpc_id = aws_vpc.vpc.id
-  
+
   tags = {
     "Name" = "mvl_ig"
   }
@@ -44,12 +43,12 @@ resource "aws_internet_gateway" "ig" {
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.vpc.id
-  
-  route = {
-    cidr_block = "0.0.0.0/0"    
+
+  route {
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.ig.id
   }
-  
+
   tags = {
     "Name" = "public"
   }
@@ -57,7 +56,7 @@ resource "aws_route_table" "public" {
 
 resource "aws_route_table_association" "public_association" {
   for_each = { for k, v in aws_subnet.public_subnet : k => v }
-  subnet_id = each.values.id
+  subnet_id = each.value.id
   route_table_id = aws_route_table.public.id
 }
 
@@ -69,10 +68,10 @@ resource "aws_nat_gateway" "public" {
   depends_on = [
     aws_internet_gateway.ig
   ]
-  
+
   allocation_id = aws_eip.nat.id
   subnet_id = aws_subnet.public_subnet[0].id
-  
+
   tags = {
     Name = "public NAT"
   }
@@ -80,12 +79,16 @@ resource "aws_nat_gateway" "public" {
 
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.vpc.id
-  
-  route = {
-    cidr_block = "0.0.0.0/0"    
-    gateway_id = aws_nat_gateway.public.id
+
+  depends_on = [
+    aws_internet_gateway.ig
+  ]
+
+   route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ig.id
   }
-  
+
   tags = {
     "Name" = "private"
   }
@@ -93,6 +96,6 @@ resource "aws_route_table" "private" {
 
 resource "aws_route_table_association" "public_private" {
   for_each = { for k, v in aws_subnet.private_subnet : k => v }
-  subnet_id = each.values.id
+  subnet_id = each.value.id
   route_table_id = aws_route_table.private.id
 }
